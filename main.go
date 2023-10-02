a7479602d20b34548dea21be5f57ecd4728e3639
Mohannad Alrasheed <43524721+Hawzen@users.noreply.github.com>
2023-10-02 20:36:21 +0300

    create server that reads and writes http requests
    still doesnt validates reads

--- main.go
+++ main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
+	"strings"
 )
 
 const (
-	CONN_PORT = "9121"
-	CONN_TYPE = "tcp"
+	// General constants
+	KILOBYTE = 1024
+	// Connection constants
+	CONN_PORT             = "9121"
+	CONN_TYPE             = "tcp"
+	RECIEVING_BUFFER_SIZE = KILOBYTE * 4
+	// HTTP constants
+	CLRF     = "\r\n"
+	NEW_LINE = "\n"
+	COLON    = ":"
 )
 
 func main() {
@@ -29,14 +39,72 @@ func main() {
 }
 
 func handleRequest(conn net.Conn) {
-	// Just print whatever you receive
-	buf := make([]byte, 1024)
+	// TODO: Establish deadline for reading/writing
+	defer conn.Close()
+
+	// Reading the request
+	request, err := handleRead(conn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("RECIEVED" + COLON + NEW_LINE + request)
+
+	// Writing the response
+	response, err := handleWrite(conn, request)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("SENDING" + COLON + NEW_LINE + response + NEW_LINE + "_______________________")
+
+}
+
+func handleRead(conn net.Conn) (string, error) {
+	// TODO: Validate request
+	// TODO: Return the request as a struct
+	buf := make([]byte, RECIEVING_BUFFER_SIZE)
+	var request bytes.Buffer
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
-			fmt.Println(err)
+			return "", err
+		}
+		request.Write(buf[:n])
+		if strings.HasSuffix(request.String(), CLRF+CLRF) {
 			break
 		}
-		fmt.Printf("Received: %s\n", buf[:n])
 	}
+	return request.String(), nil
+}
+
+func handleWrite(conn net.Conn, message string) (string, error) {
+	// TODO: Return the response as a struct
+	var response_builder strings.Builder
+	status_line := "HTTP/1.1 200 OK" + NEW_LINE
+	payload := "Hello World, go! " + NEW_LINE + "رواية البؤساء"
+
+	headers := map[string]string{
+		"Content-Type":   "text/plain; charset=utf-8",
+		"Content-Length": fmt.Sprintf("%d", len(payload)),
+	}
+
+	response_builder.WriteString(status_line)
+	for k, v := range headers {
+		response_builder.WriteString(k)
+		response_builder.WriteString(COLON)
+		response_builder.WriteString(v)
+		response_builder.WriteString(NEW_LINE)
+	}
+	response_builder.WriteString(CLRF)
+	response_builder.WriteString(payload)
+
+	response := response_builder.String()
+
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		return "", err
+	}
+
+	return response, nil
 }
