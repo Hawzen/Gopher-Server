116ab45eb4172fa08fecf805a3c719cc7d7e7bfa
Mohannad Alrasheed <43524721+Hawzen@users.noreply.github.com>
2023-10-02 14:34:46 +0300

    create server that redirects to stdout

--- main.go
+++ main.go
@@ -1,7 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net"
+)
+
+const (
+	CONN_PORT = "9121"
+	CONN_TYPE = "tcp"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	listener, err := net.Listen(CONN_TYPE, ":"+CONN_PORT)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port " + CONN_PORT + " ...")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+		}
+		go handleRequest(conn)
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	// Just print whatever you receive
+	buf := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println(err)
+			break
+		}
+		fmt.Printf("Received: %s\n", buf[:n])
+	}
 }
